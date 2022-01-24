f4ede19927a209d4c62b4ea9a48c7beb9c72815d
Heylga <oy.golovina@gmail.com>
2022-01-24 15:25:13 +0000

    Distribution folders

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,28 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import "../styles/Button.css";
+import "../styles/Card.css";
+import "../styles/Footer.css";
+import "../styles/Form.css";
+import "../styles/Navbar.css";
+import "../styles/ProfileCard.css";
+import "../styles/TextContainer.css";
+
 
 //import your own components
 import Home from "./component/home.jsx";
+import Button from "./component/Button.jsx";
+import Card from "./component/Card.jsx";
+import Footer from "./component/Footer.jsx";
+import Form from "./component/Form.jsx";
+import Navbar from "./component/Navbar.jsx";
+import ProfileCard from "./component/ProfileCard.jsx";
+import TextContainer from "./component/TextContainer.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+
+
+
