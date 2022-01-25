9973ca2cbc35b730a5ecdb0f0bea1f02daf83393
Baldonak <victormaynougomez@gmail.com>
2022-01-25 15:08:00 +0000

    Initial commit

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import * as PropTypes from "prop-types";
+import "../../styles/ProfileCard.css";
+
+const ProfileCard = (props) => {
+	return (
+		<>
+			<div class="container">
+				<h1>{props.name}</h1>
+				<p class="description_text">{props.descriptionText}</p>
+				<img
+					src={props.urlImage}
+					alt={props.altImage}
+					class={props.typeStyle === "1" ? "floatLeft" : "floatRight"} />
+			</div>
+		</>
+	);
+};
+
+ProfileCard.PropTypes = {
+	name: PropTypes.string,
+	descriptionText: PropTypes.string,
+	urlImage: PropTypes.string,
+	altImage: PropTypes.string,
+    typeStyle: PropTypes.string,
+};
+
+export default ProfileCard;
---
+++ src/js/view/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//import Footer component
+import Footer from "../component/Footer";
+
+//create your first component
+const Main = () => {
+	return (
+		<Footer 
+		name="hola"
+		
+		/>
+	);
+};
+
+export default Main;
