840d963897a4a4031070f09bd224c97d8beb2c57
Programisto1011 <victormaynougomez@gmail.com>
2022-01-26 17:05:10 +0000

    Need imprement checkout function

--- src/js/component/Form.jsx
+++ src/js/component/Form.jsx
@@ -9,35 +9,37 @@ import "../../styles/Form.css";
 const IForm = (props) => {
 	return (
 		<>
-			<Form>
-				<Form.Group className="mb-3">
-					<Form.Label>Name</Form.Label>
-					<Form.Control type="text" placeholder="Enter name" />
-				</Form.Group>
-				<Form.Group className="mb-3">
-					<Form.Label>Email address</Form.Label>
-					<Form.Control type="email" placeholder="Enter email" />
-				</Form.Group>
-				<Form.Group className="mb-3">
-					<Form.Label>Phone number</Form.Label>
-					<Form.Control
-						type="email"
-						placeholder="Enter phone number"
-					/>
-				</Form.Group>
-				<Form.Group className="mb-3">
-					<Form.Check
-						type="checkbox"
-						label="Aceptar las condiciones de uso"
-					/>
-				</Form.Group>
-				<input
+			<div class="container-fluid bg-light m-3 mr-3">
+				<Form>
+					<Form.Group className="mb-3 ml-3 ">
+						<Form.Label>Name</Form.Label>
+						<Form.Control type="text" placeholder="Enter name" />
+					</Form.Group>
+					<Form.Group className="mb-3 ml-3 ">
+						<Form.Label>Email address</Form.Label>
+						<Form.Control type="email" placeholder="Enter email" />
+					</Form.Group>
+					<Form.Group className="mb-3 ml-3 ">
+						<Form.Label>Phone number</Form.Label>
+						<Form.Control
+							type="number"
+							placeholder="Enter phone number"
+						/>
+					</Form.Group>
+					<Form.Group className="mb-3 ml-3">
+						<Form.Check
+							type="checkbox"
+							label="Aceptar las condiciones de uso"
+						/>
+					</Form.Group>
+					{/* <input
 					onChange={(e) => this.setState({ value: e.target.value })}
 					value={this.state.value}
 				/>
-				<button disabled={!this.value} />
-				<Button type="submit">Submit</Button>
-			</Form>
+				<button disabled={!this.value} /> */}
+					<Button type="submit">Submit</Button>
+				</Form>
+			</div>
 		</>
 	);
 };
