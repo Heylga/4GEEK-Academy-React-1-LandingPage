77acccaf7cb0c6adb7f8d04ada90c74b9b216700
Heylga <oy.golovina@gmail.com>
2022-01-26 15:10:22 +0000

    Card modefied

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -11,19 +11,19 @@ const Card = (props) => {
 						<div className="carduno col-md-4">
 							<img src="https://image.shutterstock.com/image-vector/electric-power-vector-icon-260nw-1140395627.jpg" />
 							<h5 class="card-title"> Power </h5>
-							<p class="card-text"> .... </p>
+							<p class="card-text"> Hello hello hello </p>
 						</div>
 
 						<div className="carddos col-md-4">
 							<img src="https://image.shutterstock.com/image-vector/electric-power-vector-icon-260nw-1140395627.jpg" />
 							<h5 class="card-title"> Power 2 </h5>
-							<p class="card-text"> .... </p>
+							<p class="card-text"> Hello hello hello </p>
 						</div>
 
 						<div className="cardtres col-md-4">
 							<img src="https://image.shutterstock.com/image-vector/electric-power-vector-icon-260nw-1140395627.jpg" />
 							<h5 class="card-title"> Power 3 </h5>
-							<p class="card-text"> .... </p>
+							<p class="card-text"> Hello hello hello </p>
 						</div>
 					</div>
 				</div>
--- src/js/view/Main.jsx
+++ src/js/view/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
 
 	return (
 		<div>
-			<Navbar className="menu"></Navbar>
+			{/* <Navbar className="menu"></Navbar> */}
 			<Card></Card>
 		</div>
 	);
