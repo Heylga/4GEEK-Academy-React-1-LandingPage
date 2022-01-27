469cbbc0c4a4f91df39c1e83aefc7a8d4c1f17cd
Programisto1011 <victormaynougomez@gmail.com>
2022-01-27 08:50:27 +0000

    First version

--- src/js/component/Form.jsx
+++ src/js/component/Form.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import * as PropTypes from "prop-types";
 import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 
 import "../../styles/Form.css";
@@ -9,37 +10,49 @@ import "../../styles/Form.css";
 const IForm = (props) => {
 	return (
 		<>
-			<div class="container-fluid bg-light m-3 mr-3">
-				<Form>
-					<Form.Group className="mb-3 ml-3 ">
-						<Form.Label>Name</Form.Label>
-						<Form.Control type="text" placeholder="Enter name" />
-					</Form.Group>
-					<Form.Group className="mb-3 ml-3 ">
-						<Form.Label>Email address</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" />
-					</Form.Group>
-					<Form.Group className="mb-3 ml-3 ">
-						<Form.Label>Phone number</Form.Label>
-						<Form.Control
-							type="number"
-							placeholder="Enter phone number"
-						/>
-					</Form.Group>
-					<Form.Group className="mb-3 ml-3">
-						<Form.Check
-							type="checkbox"
-							label="Aceptar las condiciones de uso"
-						/>
-					</Form.Group>
-					{/* <input
+			<Row className="justify-content-center">
+				<div class="container-fluid col-8 bg-light m-3 mr-3">
+					<Form>
+						<h2 class="text-center">Solicitar más información</h2>
+						<p class="text-justify font-weight-light">
+							Dejenos sus datos y contactaremos con usted
+						</p>
+						<Form.Group className="mb-3 ml-3 mt-3">
+							<Form.Label>Name</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Enter name"
+							/>
+						</Form.Group>
+						<Form.Group className="mb-3 ml-3 ">
+							<Form.Label>Email address</Form.Label>
+							<Form.Control
+								type="email"
+								placeholder="Enter email"
+							/>
+						</Form.Group>
+						<Form.Group className="mb-3 ml-3 ">
+							<Form.Label>Phone number</Form.Label>
+							<Form.Control
+								type="number"
+								placeholder="Enter phone number"
+							/>
+						</Form.Group>
+						<Form.Group className="mb-3 ml-3">
+							<Form.Check
+								type="checkbox"
+								label="Aceptar las condiciones de uso"
+							/>
+						</Form.Group>
+						{/* <input
 					onChange={(e) => this.setState({ value: e.target.value })}
 					value={this.state.value}
 				/>
 				<button disabled={!this.value} /> */}
-					<Button type="submit">Submit</Button>
-				</Form>
-			</div>
+						<Button type="submit">Submit</Button>
+					</Form>
+				</div>
+			</Row>
 		</>
 	);
 };
