e6bd815c5a60b8597c91a13eca3c49bfd8ac975f
Heylga <oy.golovina@gmail.com>
2022-01-27 08:52:10 +0000

    Last changes of Cars are done

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -14,20 +14,40 @@ const Card = (props) => {
 					<div className="col-md-12">
 						<div className="carduno col-md-4">
 							<img src="results.jpg" />
-							<h5 class="card-title"> Power </h5>
-							<p class="card-text"> Hello hello hello </p>
+							<h5 class="card-title">
+								<strong>
+									ELEGANT <br />
+									RESULTS
+								</strong>
+							</h5>
+							<p class="card-text">
+								We provide you with <br /> successful & elegant
+								<br /> results
+							</p>
 						</div>
 
 						<div className="carddos col-md-4">
 							<img src="optimization.jpg" />
-							<h5 class="card-title"> Power 2 </h5>
-							<p class="card-text"> Hello hello hello </p>
+							<h5 class="card-title">
+								<strong>OPTIMIZATION</strong>
+							</h5>
+							<p class="card-text">
+								Optimized solutions <br /> & <br /> continuos
+								improvement
+							</p>
 						</div>
 
 						<div className="cardtres col-md-4">
 							<img src="teamworking.jpg" />
-							<h5 class="card-title"> Power 3 </h5>
-							<p class="card-text"> Hello hello hello </p>
+							<h5 class="card-title">
+								<strong>
+									COMMITED TO WIN <br /> TOGETHER
+								</strong>
+							</h5>
+							<p class="card-text">
+								Staying together is progress, <br /> Working
+								together is success
+							</p>
 						</div>
 					</div>
 				</div>
